e2f26a218c79da27038271490bd49b77d70f2be2
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-05-18 12:30:58 +0100

    Adding a Modal Sheet & Understanding Context

--- lib/app/expense.dart
+++ lib/app/expense.dart
@@ -25,13 +25,21 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const Text('Modal Bottom Sheet'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expenses Tracker'),
         actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+          IconButton(
+              onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add)),
         ],
       ),
       body: Column(
