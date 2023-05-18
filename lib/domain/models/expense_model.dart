3e23c97bd1a5eb228a0354507b4328f66c5279ab
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-05-18 11:10:39 +0100

    Utilisation des icônes et formatage des dates

--- lib/domain/models/expense_model.dart
+++ lib/domain/models/expense_model.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker_app/utils/constant.dart';
+import 'package:flutter/material.dart';
 
 class Expense {
   Expense({
@@ -6,17 +7,28 @@ class Expense {
     required this.amount,
     required this.date,
     required this.category,
-  }) : id = uuid
-            .v4(); // générer identifiant unique et l'attribué comme valeur à la proprieté id
+    // uuid.v4() -> générer identifiant unique et l'attribué comme valeur à la proprieté id
+  }) : id = uuid.v4();
 
-  final String
-      id; // crée cet identifiant unique de manière dynamique à chaque fois qu'un nouvel objet Expense est créé
+  // id -> crée cet identifiant unique de manière dynamique à chaque fois qu'un nouvel objet Expense est créé
+  final String id;
   final String title;
   final double amount;
   final DateTime date;
   final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
 
 // définir un ensemble de valeurs constantes qui représentent un type spécifique.
 /// utilisées dans des conditions, des boucles ou des structures de données
 enum Category { food, travel, leisure, work }
+
+const Map<Category, IconData> categoryIcons = {
+  Category.food: Icons.launch_outlined,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,4 +1,8 @@
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 
 // générer les identifiants uniques
 const uuid = Uuid();
+
+// formater la date
+final formatter = DateFormat.yMd();
