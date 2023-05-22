c249b2edb322e5d745f9f572b4e67c6df16491e5
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-05-22 11:53:02 +0100

    Verrouillage de l'orientation de l'appareil

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:expense_tracker_app/app/expense.dart';
 import 'package:expense_tracker_app/config/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: kTheme,
-      darkTheme: kDarkTheme,
-      // themeMode: ThemeMode.system, // default
-      home: const Expenses(),
-    ),
-  );
+  // s'assurer que le verrouillage de l'orientation de l'écran et l'execution de l'app functionne comme prévu
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp], // définir l'orientation de l'app
+  ).then((fn) {
+    // appliquer l'orientation de l'app avant l'execution
+    runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: kTheme,
+        darkTheme: kDarkTheme,
+        // themeMode: ThemeMode.system, // default
+        home: const Expenses(),
+      ),
+    );
+  });
 }
